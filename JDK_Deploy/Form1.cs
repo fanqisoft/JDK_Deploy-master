9affbbb66715ae5fef18d74e8af12aef6ca85357
fanqi <fanqi@ORDINARY>
2018-04-19 17:22:05 +0800

    添加项目文件。

---
+++ JDK_Deploy/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace JDK_Deploy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.Description = "请选择JDK安装路径";
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+        /// <summary>
+        /// 写入环境变量点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("请选择JDK的安装路径！", "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (radioButton1.Checked == false && radioButton2.Checked == false)
+            {
+                MessageBox.Show("请选择写入变量的位置！", "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string jdkpath = textBox1.Text;
+            if(radioButton1.Checked) //系统变量
+            {
+                VariateRegHelp.AddJavaHome(Variate.Sys, jdkpath);
+                VariateRegHelp.AddPathAppend(Variate.Sys, @"%JAVA_HOME%\bin");
+                VariateRegHelp.AddPathAppend(Variate.Sys, @"%JAVA_HOME%\jre\bin");
+                VariateRegHelp.AddClassPath(Variate.Sys, @".");
+                VariateRegHelp.AddClassPath(Variate.Sys, @"%JAVA_HOME%\lib");
+                VariateRegHelp.AddClassPath(Variate.Sys, @"%JAVA_HOME%\lib\tools.jar");
+            }
+            if(radioButton2.Checked) //用户变量
+            {
+                VariateRegHelp.AddJavaHome(Variate.User, jdkpath);
+                VariateRegHelp.AddPathAppend(Variate.User, @"%JAVA_HOME%\bin");
+                VariateRegHelp.AddPathAppend(Variate.User, @"%JAVA_HOME%\jre\bin");
+                VariateRegHelp.AddClassPath(Variate.User, @".");
+                VariateRegHelp.AddClassPath(Variate.User, @"%JAVA_HOME%\lib");
+                VariateRegHelp.AddClassPath(Variate.User, @"%JAVA_HOME%\lib\tools.jar");
+            }
+            MessageBox.Show("JDK变量已成功写入！", "写入成功!", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd.exe", "/k java -version");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration dec =  doc.CreateXmlDeclaration("1.0", "utf-8", null);
+            doc.AppendChild(dec);
+            XmlElement variate =  doc.CreateElement("Variate");
+            doc.AppendChild(variate);
+
+            //用户Start
+            XmlElement user = doc.CreateElement("User");
+            variate.AppendChild(user);
+            XmlElement userPath = doc.CreateElement("path");
+            userPath.InnerText = VariateRegHelp.GetEnvironmentByName(Variate.User, "PATH");
+            user.AppendChild(userPath);
+
+            //系统Start
+            XmlElement sys = doc.CreateElement("Sys");
+            variate.AppendChild(sys);
+            XmlElement sysPath = doc.CreateElement("path");
+            sysPath.InnerText = VariateRegHelp.GetEnvironmentByName(Variate.Sys, "PATH");
+            sys.AppendChild(sysPath);
+
+            string fileName = DateTime.Now.ToString("yyyy-MM-dd HHmmss") + ".xml";
+            doc.Save(fileName);
+            MessageBox.Show("环境变量已成功备份！", "备份成功!", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string fileName = string.Empty;
+            openFileDialog1.InitialDirectory = System.Environment.CurrentDirectory;
+            openFileDialog1.Filter = "XML文件|*.xml";
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.FilterIndex = 1;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(fileName);
+            XmlNode xn = doc.SelectSingleNode("/Variate/User/path");
+            string msg = xn.InnerText;
+            VariateRegHelp.SetEnvironment(Variate.User, "PATH", msg);
+            xn = doc.SelectSingleNode("/Variate/Sys/path");
+            msg = xn.InnerText;
+            VariateRegHelp.SetEnvironment(Variate.Sys, "PATH", msg);
+            MessageBox.Show("环境变量已成功还原！", "还原成功!", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+    }
+}
---
+++ JDK_Deploy/VariateRegHelp.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDK_Deploy
+{
+    public enum Variate
+    {
+        Sys,
+        User,
+    }
+    public class VariateRegHelp
+    {
+        /// <summary>
+        /// 根据名称获取指定的环境变量
+        /// </summary>
+        /// <param name="type">枚举环境变量</param>
+        /// <param name="name">变量名称</param>
+        /// <returns></returns>
+        public static string GetEnvironmentByName(Variate type, string name)
+        {
+            string result = string.Empty;
+            try
+            {
+                result = OpenEnvironmentReg(type).GetValue(name).ToString();//读取
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 打开指定的环境变量注册表
+        /// </summary>
+        /// <param name="type">枚举环境变量</param>
+        /// <returns></returns>
+        private static RegistryKey OpenEnvironmentReg(Variate type)
+        {
+            RegistryKey regEnvironment = null;
+            if (type == Variate.Sys)
+            {
+                RegistryKey regLocalMachine = Registry.LocalMachine;
+                RegistryKey regSYSTEM = regLocalMachine.OpenSubKey("SYSTEM", true);//打开HKEY_LOCAL_MACHINE下的SYSTEM 
+                RegistryKey regControlSet001 = regSYSTEM.OpenSubKey("ControlSet001", true);//打开ControlSet001 
+                RegistryKey regControl = regControlSet001.OpenSubKey("Control", true);//打开Control 
+                RegistryKey regManager = regControl.OpenSubKey("Session Manager", true);//打开Control 
+                regEnvironment = regManager.OpenSubKey("Environment", true);
+            }
+            else if (type == Variate.User)
+            {
+                RegistryKey regCurrentUser = Registry.CurrentUser;
+                regEnvironment = regCurrentUser.OpenSubKey("Environment", true);
+            }
+            return regEnvironment;
+        }
+
+        /// <summary>
+        /// 设置指定的环境变量
+        /// </summary>
+        /// <param name="type">枚举环境变量</param>
+        /// <param name="name">变量名称</param>
+        /// <param name="strValue">变量值</param>
+        public static void SetEnvironment(Variate type, string name, string strValue)
+        {
+            OpenEnvironmentReg(type).SetValue(name, strValue);
+
+        }
+
+        /// <summary>
+        /// 检测指定的环境变量是否存在指定变量
+        /// </summary>
+        /// <param name="type">枚举环境变量</param>
+        /// <param name="name">变量名称</param>
+        /// <returns>存在则返回true，否则返回false</returns>
+        public static bool CheckEnvironmentExist(Variate type, string name)
+        {
+            if (!string.IsNullOrEmpty(GetEnvironmentByName(type, name)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// 添加到JAVA_HOME环境变量（检测值是否存在，存在则不添加）
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="jdkpath"></param>
+        public static void AddJavaHome(Variate type, string jdkpath)
+        {
+            bool exist = CheckEnvironmentExist(type,"JAVA_HOME");
+            if (exist)  //如果存在
+            {
+                string javaHome = GetEnvironmentByName(type,"JAVA_HOME");
+                if (javaHome.Substring(javaHome.Length - 1, 1) != ";")
+                {
+                    SetEnvironment(type,"JAVA_HOME", javaHome + ";");
+                    javaHome = GetEnvironmentByName(type,"JAVA_HOME");
+                }
+                string[] list = javaHome.Split(';');
+                foreach (var item in list)
+                {
+                    if (item == jdkpath)
+                    {
+                        return;
+                    }
+                }
+                SetEnvironment(type,"JAVA_HOME", javaHome + jdkpath + ";");
+
+            }
+            else //如果不存在
+            {
+                SetEnvironment(type,"JAVA_HOME", jdkpath + ";");
+            }
+        }
+
+
+        /// <summary>
+        /// 添加到PATH环境变量（检测值是否存在，存在则不添加）
+        /// </summary>
+        /// <param name="strHome"></param>
+        public static void AddPathAppend(Variate type, string strHome)
+        {
+            string pathlist;
+            pathlist = GetEnvironmentByName(type, "PATH");
+            //检测是否以;结尾
+            if (pathlist.Substring(pathlist.Length - 1, 1) != ";")
+            {
+                SetEnvironment(type, "PATH", pathlist + ";");
+                pathlist = GetEnvironmentByName(type, "PATH");
+            }
+            string[] list = pathlist.Split(';');
+            bool isPathExist = false;    //判断附加的值是否已存在
+
+            foreach (string item in list)
+            {
+                if (item == strHome)
+                    isPathExist = true;
+            }
+            if (!isPathExist)
+            {
+                SetEnvironment(type, "PATH", pathlist + strHome + ";");
+            }
+        }
+
+        public static void AddClassPath(Variate type, string path)
+        {
+            bool exist = CheckEnvironmentExist(type, "CLASSPATH");
+            if (exist)  //如果存在
+            {
+                string classPath = GetEnvironmentByName(type, "CLASSPATH");
+                if (classPath.Substring(classPath.Length - 1, 1) != ";")
+                {
+                    SetEnvironment(type, "CLASSPATH", classPath + ";");
+                    classPath = GetEnvironmentByName(type, "CLASSPATH");
+                }
+                string[] list = classPath.Split(';');
+                foreach (var item in list)
+                {
+                    if (item == path)
+                    {
+                        return;
+                    }
+                }
+                SetEnvironment(type, "CLASSPATH", classPath + path + ";");
+
+            }
+            else //如果不存在
+            {
+                SetEnvironment(type, "CLASSPATH", path + ";");
+            }
+        }
+    }
+}
